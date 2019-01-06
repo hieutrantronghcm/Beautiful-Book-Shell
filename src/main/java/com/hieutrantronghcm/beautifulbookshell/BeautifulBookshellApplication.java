aee1a799ce3755308f62a60a96d359491427b54a
hieutrantronghcm <hieutrantronghcm@gmail.com>
2019-01-06 19:38:51 +0700

    create project

---
+++ src/main/java/com/hieutrantronghcm/beautifulbookshell/BeautifulBookshellApplication.java
@@ -0,0 +1,14 @@
+package com.hieutrantronghcm.beautifulbookshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulBookshellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautifulBookshellApplication.class, args);
+    }
+
+}
+
